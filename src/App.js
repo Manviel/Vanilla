efe0bc806d7849745c8f8b162fe7ecbb93fc15fe
Mike <Mihaylo.Merezhko@kname.edu.ua>
2017-08-24 17:35:48 +0300

    npm update

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import Form from './components/Form';
-import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
-
 class App extends Component {
-  state = {
-    mounted: true
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    this.setState({ mounted: false });
-  };
-
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <div className="App-intro">
-          <CSSTransitionGroup
-            transitionName="fade"
-            transitionAppear={true}
-            transitionAppearTimeout={500}
-            transitionEnter={false}
-            transitionLeaveTimeout={300}>
-            {this.state.mounted && <Form onSubmit={this.handleSubmit} />}
-          </CSSTransitionGroup>
+        <div className="landing">
+          <h1>sVideo</h1>
+          <input type="text" placeholder="Search" />
         </div>
       </div>
     );
