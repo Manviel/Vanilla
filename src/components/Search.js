7be630589cc646a818a9e257c65ba3ec6a8e3824
Mike <Mihaylo.Merezhko@kname.edu.ua>
2017-08-26 17:48:13 +0300

    State and Filtering

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import ShowCard from './ShowCard';
 import preload from '../data/data.json';
 
-const Search = () => {
-  return(
-    <div className='search'>
-      <div className='container'>
-        {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+class Search extends Component {
+  state = {
+    searchTerm: ''
+  };
+
+  handleSearchTerm = event => {
+    this.setState({ searchTerm: event.target.value })
+  };
+
+  render() {
+    return(
+      <div className='search'>
+        <header className="header">
+          <span>sVideo</span>
+          <input onChange={this.handleSearchTerm}
+            value={this.state.searchTerm}
+            type="text" placeholder="Search" />
+        </header>
+        <div className='container'>
+          {preload.shows
+            .filter(
+              show =>
+                `${show.title} ${show.description}`.toUpperCase()
+                  .indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            )
+            .map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Search;
