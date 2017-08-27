c1d6916c0046009d8a1d0750420fbbb92c2a449b
Mike <Mihaylo.Merezhko@kname.edu.ua>
2017-08-27 18:57:05 +0300

    Spinner component

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,30 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
+import Spinner from './Spinner';
 
-const Details = (props) => {
-  const { title, description, year, poster, trailer } = props.show;
+class Details extends Component {
+  state = {
+    apiData: { rating: '' }
+  };
 
-  return(
-    <div className='details'>
-      <Header />
-      <section>
-        <h1>{title}</h1>
-        <h2>{year}</h2>
-        <img alt={`For ${title}`}
-          src={require(`../img/posters/${poster}`)} />
-        <p>{description}</p>
-      </section>
-      <div>
-        <iframe
-          src={`https://www.youtube-nocookie.com/embed/${trailer}
-            ?rel=0&amp;controls=0&amp;showinfo=0`}
-          frameBorder="0"
-          allowFullScreen
-          title={`Trailer for ${title}`}
-        />
+  props: {
+    show: Show
+  };
+
+  render() {
+    const { title, description, year, poster, trailer } = this.props.show;
+    let ratingComponent;
+    if (this.state.apiData.rating) {
+      ratingComponent = <h3>{this.state.apiData.rating}</h3>;
+    } else {
+      ratingComponent = <Spinner />;
+    }
+
+    return(
+      <div className='details'>
+        <Header />
+        <section>
+          <h1>{title}</h1>
+          <h2>({year})</h2>
+          {ratingComponent}
+          <img alt={`For ${title}`}
+            src={require(`../img/posters/${poster}`)} />
+          <p>{description}</p>
+        </section>
+        <div>
+          <iframe
+            src={`https://www.youtube-nocookie.com/embed/${trailer}
+              ?rel=0&amp;controls=0&amp;showinfo=0`}
+            frameBorder="0"
+            allowFullScreen
+            title={`Trailer for ${title}`}
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Details;
---
+++ src/components/Spinner.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Style.css';
+
+const Spinner = () => <img src={require(`../img/loading.png`)}
+  className="spinner" alt="loading" />
+
+export default Spinner;
