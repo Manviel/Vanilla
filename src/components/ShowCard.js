da81b14b64384e1e25ee939b3f822a7fd34a6274
Mike <Mihaylo.Merezhko@kname.edu.ua>
2017-08-28 13:20:15 +0300

    Edit json data and Perfomance

--- src/components/ShowCard.js
+++ src/components/ShowCard.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import { string } from 'prop-types';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import './Style.css';
 
-const ShowCard = (props: Show) => {
-  return(
-    <Link className='wrapper' to={`/details/${props.imdbID}`}>
-      <img className='image' alt={`${props.title} Show poster`}
-        src={require(`../img/posters/${props.poster}`)} />
-      <div>
-        <h3>{props.title}</h3>
-        <h4>({props.year})</h4>
-        <p>{props.description}</p>
-      </div>
-    </Link>
-  );
-}
+class ShowCard extends Component {
+  shouldComponentUpdate() {
+    return false;
+  }
+  props: Show;
 
-ShowCard.propTypes = {
-  poster: string.isRequired,
-  title: string.isRequired,
-  year: string.isRequired,
-  description: string.isRequired
-};
+  render() {
+    return(
+      <Link className='wrapper' to={`/details/${this.props.imdbID}`}>
+        <img className='image' alt={`${this.props.title} Show poster`}
+          src={require(`../img/posters/${this.props.poster}`)} />
+        <div>
+          <h3>{this.props.title}</h3>
+          <h4>({this.props.year})</h4>
+          <p>{this.props.description}</p>
+        </div>
+      </Link>
+    );
+  }
+}
 
 export default ShowCard;
