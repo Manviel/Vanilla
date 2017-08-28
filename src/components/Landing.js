25e3635d1a3164d88baa3c0fe84f1b5484b2039d
Mike <Mihaylo.Merezhko@kname.edu.ua>
2017-08-28 16:16:11 +0300

    Interact and Dispatching

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,16 +1,29 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import './Style.css';
 
-const Landing = () => {
+import { setSearchTerm } from '../redux/actionCreators';
+
+const Landing = (props) => {
   return(
     <div className="landing">
       <h1>sVideo</h1>
-      <input type="text" placeholder="Search" />
+      <input onChange={props.handleSearchTerm} value={props.searchTerm}
+        type="text" placeholder="Search" />
       <Link to="/search" className="link">Browse</Link>
     </div>
   );
-}
+};
 
-export default Landing;
+const mapStateToProps = state => ({
+  searchTerm: state.searchTerm
+});
+
+const mapDispatchToProps = (dispatch: Function) => ({
+  handleSearchTerm(event) {
+    dispatch(setSearchTerm(event.target.value));
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Landing);
