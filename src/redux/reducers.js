838bedaf65336ca3c2add830a3123e8a0340e5d4
Mike <Mihaylo.Merezhko@kname.edu.ua>
2017-08-29 15:33:31 +0300

    Install axios. Async functions

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,13 +1,20 @@
 import { combineReducers } from 'redux';
-import { SET_SEARCH_TERM } from './actions';
+import { SET_SEARCH_TERM, ADD_API_DATA } from './actions';
 
-const searchTerm = (state='', action: Action) => {
+const searchTerm = (state = '', action: Action) => {
   if (action.type === SET_SEARCH_TERM) {
     return action.payload;
   }
   return state;
-}
+};
 
-const roorReducer = combineReducers({ searchTerm: searchTerm });
+const apiData = (state = {}, action: Action) => {
+  if (action.type === ADD_API_DATA) {
+    return Object.assign({}, state, { [action.payload.imdbID]: action.payload });
+  }
+  return state;
+};
+
+const roorReducer = combineReducers({ searchTerm, apiData });
 
 export default roorReducer;
