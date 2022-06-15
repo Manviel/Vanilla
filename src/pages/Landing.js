6511eab3dabf27fab6903263d3135b88c27381d4
mmerezhko <mmere@softserveinc.com>
2022-06-15 15:14:08 +0300

    Refactor this func to reduce Cognitive Complexity

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,13 +1,13 @@
-import React, { lazy, useContext, useEffect, Suspense } from "react";
-import { Link } from "react-router-dom";
+import React, { lazy, useContext, useEffect, Suspense } from 'react';
+import { Link } from 'react-router-dom';
 
-import Loader from "../components/Loader";
+import Loader from '../components/Loader';
 
-import { DataContext } from "../utils/context";
+import { DataContext } from '../utils/context';
 
-const Filters = lazy(() => import("../components/Filters"));
-const Header = lazy(() => import("../components/Header"));
-const ShowCard = lazy(() => import("../components/ShowCard"));
+const Filters = lazy(() => import('../components/Filters'));
+const Header = lazy(() => import('../components/Header'));
+const ShowCard = lazy(() => import('../components/ShowCard'));
 
 const Landing = () => {
   const { state, dispatch } = useContext(DataContext);
@@ -18,43 +18,40 @@ const Landing = () => {
 
   const getData = () => {
     if (state.data.length === 0) {
-      fetch("https://jsonplaceholder.typicode.com/posts")
+      fetch('https://jsonplaceholder.typicode.com/posts')
         .then((response) => response.json())
-        .then((json) => dispatch({ type: "update", payload: json }));
+        .then((json) => dispatch({ type: 'update', payload: json }));
     }
   };
 
   const handleFilter = (query) => {
-    if (query === "asc") {
-      const result = state.data.sort((a, b) =>
+    if (query === 'asc') {
+      return state.data.sort((a, b) =>
         a.id > b.id ? 1 : b.id > a.id ? -1 : 0
       );
-
-      dispatch({ type: "update", payload: result });
-    } else if (query === "desc") {
-      const result = state.data.sort((a, b) =>
+    } else if (query === 'desc') {
+      return state.data.sort((a, b) =>
         a.id < b.id ? 1 : b.id < a.id ? -1 : 0
       );
-
-      dispatch({ type: "update", payload: result });
     } else {
-      const result = state.data.sort((a, b) =>
+      return state.data.sort((a, b) =>
         a.title > b.title ? 1 : b.title > a.title ? -1 : 0
       );
-
-      dispatch({ type: "update", payload: result });
     }
   };
+
+  const handleResult = (result) =>
+    dispatch({ type: 'update', payload: handleFilter(result) });
 
   return (
     <Suspense fallback={<Loader />}>
       <Header />
-      <Filters handleFilter={handleFilter} />
+      <Filters handleFilter={handleResult} />
 
-      <main className="flex container">
+      <main className='flex container'>
         {state.data.map((show) => (
-          <Link key={show.id} to={`/${show.id}`} className="item">
-            <ShowCard title={show.title} />
+          <Link key={show.id} to={`/${show.id}`} className='item'>
+            <ShowCard title={show.title} price={show.id} />
           </Link>
         ))}
       </main>
