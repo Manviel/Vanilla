1e48ed46375f07e3a6ba2f7cfe9a8518196297a1
Mike <mmieriezhko@outlook.com>
2022-06-15 16:26:44 +0300

    Switch statements should have at least 3 case (#23)
    * Switch statements should have at least 3 case
    
    * Move devServer to resolve duplicate key
    
    * Options has an unknown property 'watchContentBase'

--- src/utils/context.js
+++ src/utils/context.js
@@ -1,18 +1,20 @@
-import React, { createContext, useReducer, useMemo } from "react";
+import React, { createContext, useReducer, useMemo } from 'react';
 
 export const DataContext = createContext();
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case "update":
+    case 'update':
       return { ...state, data: action.payload };
+    case 'auth':
+      return { ...state, isAllowed: action.payload };
     default:
       return { ...state };
   }
 };
 
 export const Store = (props) => {
-  const [state, dispatch] = useReducer(reducer, { data: [] });
+  const [state, dispatch] = useReducer(reducer, { data: [], isAllowed: false });
 
   const contextValue = useMemo(() => ({ state, dispatch }), [state]);
 
